83ed4379cbd2bd116f796c95a42be9481f2f3931
Akbar <abdullaev.akbar@gmail.com>
2022-09-14 10:30:35 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = '5627423398:AAHyMncrkbTHgxd1tpok0eT9zFi9INLzIS8'
+APIKey = '&api_key=013fade4a9fc0df00eb5c77266f991bd3836ecc36b8d5895ba1fced843fc6a0a'
+currencies = {'доллар': 'USD',
+              'фунт': 'GBP',
+              'евро': 'EUR',
+              'рубль': 'RUB',
+              'биткоин': 'BTC',
+              'эфириум': 'ETC'}
